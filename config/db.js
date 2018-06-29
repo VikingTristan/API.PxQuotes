fda55743eb209c3a2714bc041d96af5b88d047df
Thomas Lorntzen <thomas.lorntzen@payex.com>
2018-06-29 14:18:23 +0200

    Added config/db.js

---
+++ config/db.js
@@ -0,0 +1,20 @@
+//Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
+const mongoose = require("mongoose");
+
+const options = {
+    reconnectTries: Number.MAX_VALUE,
+    // useMongoClient: true,
+    poolSize: 10
+};
+
+console.log("Attempting to connect to " + process.env.DB_URI );
+
+mongoose.connect(process.env.DB_URI, options).then(
+    () => {
+        console.log("Database connection established.");
+
+    },
+    err => {
+        console.log("Error connecting to database instance due to: ", err);
+    }
+);
